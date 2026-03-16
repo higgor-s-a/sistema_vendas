4328a503964a319b2d9c8c09e973e83c77c3559e
higgor-s-a <higgor.sampaio@gmail.com>
2026-03-16 20:43:03 -0300

    correção de horários

--- services/log_service.py
+++ services/log_service.py
@@ -7,7 +7,9 @@ def registrar_log(usuario, acao, detalhes=""):
     sheet = conectar_sheet()
     aba = sheet.worksheet("logs")
 
-    data_hora = datetime.now().strftime("%d/%m/%Y %H:%M")
+    from zoneinfo import ZoneInfo
+    data_hora = datetime.now(
+        ZoneInfo("America/Sao_Paulo")).strftime("%d/%m/%Y %H:%M")
 
     aba.append_row([
         data_hora,
